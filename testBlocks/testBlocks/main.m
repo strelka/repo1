8004574b9d1129c16182d631c3191f722c604331
Sharaeva J <yu.a.novoselova@gmail.com>
2017-04-15 12:23:27 +0300

    test blocks

---
+++ testBlocks/testBlocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testBlocks
+//
+//  Created by iOS-School-1 on 15.04.17.
+//  Copyright © 2017 JSh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
