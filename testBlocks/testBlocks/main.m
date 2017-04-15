0e0aba3dc72677a287f146c3da9dc7feb12049f2
Sharaeva J <yu.a.novoselova@gmail.com>
2017-04-15 13:38:40 +0300

    magic weak/strong

--- testBlocks/testBlocks/main.m
+++ testBlocks/testBlocks/main.m
@@ -13,30 +13,66 @@
 //
 //}
 
+@interface BlockTestClass : NSObject
+
+-(void)TestMemory;
+@end
+
+@interface BlockTestClass()
+@property(nonatomic, copy) NSString* str;
+@property (nonatomic, copy) void(^test)(void);
+@end
+@implementation BlockTestClass
+-(instancetype)init{
+    self = [super init];
+    if (self){
+        _str = @"HelloWorld";
+    
+    }
+    return self;
+
+}
+
+-(void)testMemory{
+    __weak typeof (self) weakSelf = self;
+    self.test = ^void(void){
+        __strong typeof(self) strongSelf = weakSelf;
+        
+        NSLog(@"%@", strongSelf.str);
+    };
+    self.test();
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        dispatch_sync(dispatch_get_global_queue
-                      (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            NSLog(@"Hello world!");
-        });
-                         }
-        __block int a = 0;
-    
-        int (^inc)(void) = ^int(void)
-        {
-            
-            ++a;
-            NSLog(@"%@", @(a));
-            return a;
         
-        };
-    
-    [inc copy]; // iz bloka mogno rabotat' iz vne(!??)
-//        a += 10;
+//        dispatch_sync(dispatch_get_global_queue
+//                      (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//            NSLog(@"Hello world!");
+//        });
+//                         }
+//        __block int a = 0;
+//    
+//        int (^inc)(void) = ^int(void)
+//        {
+//            
+//            ++a;
+//            NSLog(@"%@", @(a));
+//            return a;
 //        
-//        f(2, inc);
-        inc();
-
+//        };
+//    
+//    [inc copy]; // iz bloka mogno rabotat' iz vne(!??)
+////        a += 10;
+////        
+////        f(2, inc);
+//        inc();
+        BlockTestClass *test = [BlockTestClass new];
+        [test testMemory];
+        
+    }
     return 0;
 }
