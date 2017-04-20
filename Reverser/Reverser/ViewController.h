35322ef9a0f475f3cca886f9e15df26acbc961c8
Sharaeva J <yu.a.novoselova@gmail.com>
2017-04-20 19:42:57 +0300

    init autolayout

---
+++ Reverser/Reverser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reverser
+//
+//  Created by iOS-School-1 on 20.04.17.
+//  Copyright © 2017 JSh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Reverser/ReverserTests/ReverserTests.m
@@ -0,0 +1,39 @@
+//
+//  ReverserTests.m
+//  ReverserTests
+//
+//  Created by iOS-School-1 on 20.04.17.
+//  Copyright © 2017 JSh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReverserTests : XCTestCase
+
+@end
+
+@implementation ReverserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
